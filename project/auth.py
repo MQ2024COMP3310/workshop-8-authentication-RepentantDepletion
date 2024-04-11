3fcd84d1fae345d891837077421a622ec010bb9e
RepentantDepletion <isaac.bella1@students.mq.edu.au>
2024-04-11 19:19:47 +1000

    Copied quick fix for line 42 in Auth.py

--- project/auth.py
+++ project/auth.py
@@ -39,7 +39,8 @@ def signup_post():
     name = request.form.get('name')
     password = request.form.get('password')
 
-    user = db.session.execute(text('select * from user where email = "' + email + '')).all()
+    db.session.execute(text('select * from user where email = "' + email +'"')).all()
+    user = db.fetchone()
     if len(user) > 0: # if a user is found, we want to redirect back to signup page so user can try again
         flash('Email address already exists')  # 'flash' function stores a message accessible in the template code.
         app.logger.debug("User email already exists")
